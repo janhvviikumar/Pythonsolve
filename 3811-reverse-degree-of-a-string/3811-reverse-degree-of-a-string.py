141cc71985932ee8e5498053fbdb2df97cad0f3d
janhvviikumar <janhvviik@gmail.com>
2025-11-05 11:51:00 +0530

    Time: 7 ms (68.09%), Space: 17.7 MB (53.82%) - LeetHub

--- 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/3811-reverse-degree-of-a-string.py
@@ -1,10 +1,6 @@
 class Solution:
     def reverseDegree(self, s: str) -> int:
-        l=['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a']
         count=0
         for ch in range(len(s)):
-            for i in range(len(l)):
-                if s[ch]==l[i]:
-                    count+=(ch+1)*(i+1)
-                    break
+            count+=(ch+1)*((ord('z')-ord(s[ch]))+1)
         return count
